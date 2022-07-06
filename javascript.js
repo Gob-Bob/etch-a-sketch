338166b1c5ca1163699089da6eaba9953f64583e
Tyler <nguyentyler97@gmail.com>
2022-07-05 18:39:22 -0700

    Add grid container

--- javascript.js
+++ javascript.js
@@ -1,4 +1,7 @@
 const body = document.querySelector('body');
+const gridContiner = document.createElement('continer');
+gridContiner.classList.add('grid');
+body.appendChild(gridContiner)
 
 function createGridBlock(amount) {
     for (let i = 0; i < amount; i++) {
