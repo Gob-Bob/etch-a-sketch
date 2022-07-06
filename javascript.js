6be2627105c88ce153aceee6de273cb4eca55742
Tyler <nguyentyler97@gmail.com>
2022-07-05 19:50:27 -0700

    Hover color change functionality tested

--- javascript.js
+++ javascript.js
@@ -3,3 +3,18 @@ const container = document.createElement('div');
 container.classList.add('container');
 body.appendChild(container)
 
+const object = document.createElement('div');
+object.classList.add('object');
+
+object.style.border = 'medium solid black';
+object.style.width = '100px';
+object.style.height = '100px';
+
+function turnBlue() {
+    object.style.backgroundColor = 'blue';
+}
+
+object.addEventListener('mouseover', turnBlue);
+
+container.appendChild(object);
+
