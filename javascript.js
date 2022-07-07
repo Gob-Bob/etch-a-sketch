0465c0a0f8a4f1e313805741f30939822eeb8448
Tyler <nguyentyler97@gmail.com>
2022-07-06 18:51:16 -0700

    Add centered 16x16 grid

--- javascript.js
+++ javascript.js
@@ -1,30 +1,50 @@
 const body = document.querySelector('body');
 const container = document.createElement('div');
+
+body.style.display = 'flex';
+body.style.justifyContent = 'center';
+body.style.alignContent = 'center';
+
 container.classList.add('container');
-body.appendChild(container)
+container.style.border = 'medium solid black'
+container.style.display = 'grid';
+container.style.gridTemplateColumns = 'repeat(16, 40px)';
+container.style.gridTemplateRows = 'repeat(16, 40px)';
 
-
-
-function createBox(boxNum) {
-    for (let i = 0; i < boxNum; i++) {
-        const object = document.createElement('div');
-        object.classList.add('object');
-
-        object.style.border = 'medium solid black';
-        object.style.width = '100px';
-        object.style.height = '100px';
-
-        container.appendChild(object);
+function addDivs(amount) {
+    for (let i = 0; i < amount; i++) {
+        const div = document.createElement('div');
+        div.style.border = 'medium solid black';
+        // div.textContent = 'DIV';
+        container.appendChild(div);
     }
 }
+addDivs(256);
 
-createBox(16);
+body.appendChild(container);
 
-function turnBlue() {
-    this.style.backgroundColor = 'blue';
-}
+// function createBox(row, column) {
+//     for (let i = 0; i < column; i++) {
+//         for (let j = 0; j < row; j++) {
+//             const object = document.createElement('div');
+//             object.classList.add('object');
 
-const boxNodeList = document.querySelectorAll('.object');
-boxNodeList.forEach(item => {
-    item.addEventListener('mouseover', turnBlue)
-});
+//             object.style.border = 'medium solid black';
+//             object.style.width = '100px';
+//             object.style.height = '100px';
+
+//             container.appendChild(object);
+//         }
+//     }
+// }
+
+// createBox(2, 2);
+
+// function turnBlue() {
+//     this.style.backgroundColor = 'blue';
+// }
+
+// const boxNodeList = document.querySelectorAll('.object');
+// boxNodeList.forEach(item => {
+//     item.addEventListener('mouseover', turnBlue)
+// });
